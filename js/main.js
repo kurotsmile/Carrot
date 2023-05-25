f172722de926d5d6afdfa45bb7f3c52af98bc35e
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-05-25 21:06:43 +0700

    Add function App
    Add function App

---
+++ js/main.js
@@ -0,0 +1,89 @@
+
+class Carrot_form{
+    list_field;
+    list_btn;
+    event_done;
+    constructor(name) {
+        this.name=name;
+        this.list_field=Array();
+        this.list_btn=Array();
+        this.event_done=null;
+    }
+
+    add_field(name_fied){
+        this.list_field.push(name_fied);
+    }
+
+    add_btn(name_btn){
+        this.list_btn.push(name_btn);
+    }
+
+    show() {
+        var html="<div class='frm'>";
+        for (let i = 0; i < this.list_field.length; i++) {
+            html +="<div class='frm-line'>";
+            html +="<label for='"+this.list_field[i]+"'>"+this.list_field[i]+"</label>";
+            html +="<input ";
+            html +="id=\""+this.list_field[i] + "\" ";
+            html +="value=\""+this.list_field[i] + "\" ";
+            html +=" />";
+            html +="</div>";
+        }
+
+        for (let i = 0; i < this.list_btn.length; i++) {
+            html +="<div class='frm-all-btn'>";
+            html +="<input type='button' ";
+            html +="id=\""+this.list_btn[i] + "\" ";
+            html +="value=\""+this.list_btn[i] + "\" ";
+            html +="</div>";
+        }
+
+        html+='<script>$("#btn_add").click(function(){add_user();});</script>';
+        html+="</div>";
+        return html;
+    }
+
+    set_act_done(event){
+        this.event_done=event;
+    }
+}
+
+function show_all_app(){
+    const cr_form=new Carrot_form("add_app");
+    cr_form.add_field("name");
+    cr_form.add_field("icon");
+    cr_form.add_field("desc");
+    cr_form.add_btn("btn_add");
+    cr_form.set_act_done("act_done_add_app()");
+    $("#contain_body").html(cr_form.show());
+}
+
+function act_done_add_app(){
+    alert(app);
+    alert("Done");
+}
+
+function show_add_app(){
+    close_all_box();
+    $("#box_add_app").show();
+}
+
+function show_add_lang(){
+    close_all_box();
+    $("#box_add_lang").show();
+}
+
+function close_all_box(){
+    $("#box_add_app").hide();
+    $("#box_add_lang").hide();
+}
+
+$("#btn_all_user").click(async function(){
+    $("#all_user").html("");
+    var q = query(collection(db, "user"));
+    var querySnapshot =await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+        $("#all_user").append(doc.id);
+    });
+});
