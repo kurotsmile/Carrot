8122d198026f57336153de3f47e226cb8174dcd9
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-06-13 10:07:03 +0700

    Update Lang setting
    Update Lang setting

--- js/ai_lover.js
+++ js/ai_lover.js
@@ -93,11 +93,12 @@ class Ai_Lover{
     show_setting_lang(data_lang_tag,data_lang_change){
         var html = '';
         var list_lang=this.carrot.list_lang;
+        var ai_lover=this;
 
         html+='<h3>Dịch thuật đa ngôn ngữ <small class="text-muted">'+this.setting_lang_collection+'</small></h3>';
 
         $.each(list_lang,function(i,lang){
-            if(lang.key==data_lang_change.id)
+            if(lang.key==ai_lover.setting_lang_change)
                 html+='<button type="button" class="btn btn-light btn-sm mr-1 mt-1 btn-setting-lang-change" key_change="'+lang.key+'"><img src="'+lang.icon+'" style="width:20px"/>'+lang.name+'</button> ';
             else
                 html+='<button type="button" class="btn btn-secondary btn-sm mr-1 mt-1 btn-setting-lang-change" key_change="'+lang.key+'"><img src="'+lang.icon+'" style="width:20px"/>'+lang.name+'</button> ';
@@ -111,9 +112,8 @@ class Ai_Lover{
         html += '<th scope="col">New Lang</th>';
         html += '</tr>';
         html += '</thead>';
-
-        html += '<tbody>';
-        var ai_lover=this;
+        html += '<tbody id="body_table_lang_setting">';
+        
         $.each(data_lang_tag, function(key, value){
             var s_val_change='';
             if(data_lang_change!=null){
@@ -121,14 +121,20 @@ class Ai_Lover{
                     s_val_change=data_lang_change[key];
                 }
             }
+
+            if(key=='id'&&s_val_change=='') s_val_change=ai_lover.setting_lang_change;
             html += '<tr>';
             html += '<td scope="col" class="w-10"><b>'+key+'</b></td>';
-            html += '<td scope="col" class="w-25"><span id="txt_'+key+'">'+value+'</span> <button class="btn btn-outline-secondary btn-sm" type="button" onclick="copy_txt_tag(\'txt_'+key+'\')"><i class="fa-solid fa-copy"></i></button> <button class="btn btn-outline-secondary btn-sm" type="button" onclick="tr(\'txt_'+key+'\',\''+ai_lover.setting_lang_change+'\')"><i class="fa-solid fa-language"></i></button></td>';
+            html += '<td scope="col" class="w-25">';
+                html += '<span id="txt_'+key+'">'+value+'</span> '
+                if(key!='id') html += '<button class="btn btn-outline-secondary btn-sm" type="button" onclick="copy_txt_tag(\'txt_'+key+'\')"><i class="fa-solid fa-copy"></i></button> <button class="btn btn-outline-secondary btn-sm" type="button" onclick="tr(\'txt_'+key+'\',\''+ai_lover.setting_lang_change+'\')"><i class="fa-solid fa-language"></i></button>';
+            html += '</td>';
             html += '<td scope="col">';
                 html += '<div class="input-group">';
                     html += '<input id="inp_'+key+'" type="text" value="'+s_val_change+'" class="form-control inp-lang input-sm" data-key="'+key+'"/>';
                     html += '<div class="input-group-append">';
-                    html += '<button class="btn btn-outline-secondary" type="button" onclick="paste_tag(\'inp_'+key+'\')"><i class="fa-solid fa-paste"></i> Paste</button>';
+                    if(key!="id") html += '<button class="btn btn-outline-secondary" type="button" onclick="paste_tag(\'inp_'+key+'\')"><i class="fa-solid fa-paste"></i> Paste</button>';
+                    if(data_lang_change.id=="en"&&key!="id") html += '<button class="btn btn-danger" type="button" onclick=" $(this).parent().parent().parent().parent().remove();"><i class="fa-solid fa-trash"></i> Delete</button>';
                     html += '</div>';
                 html += '</div>';
             html += '</td>';
@@ -138,6 +144,34 @@ class Ai_Lover{
         html += '</table>';
 
         html+='<button id="btn_done_setting_lang" type="button" class="btn btn-primary mr-1 mt-1"><i class="fa-solid fa-square-check"></i> Done</button> ';
+        if(data_lang_change.id=="en") html+='<button id="btn_add_field_setting_lang" type="button" class="btn btn-secondary mr-1 mt-1 btn-sm" ><i class="fa-solid fa-add"></i> Add Field</button>';
         $("#main_contain").html(html);
+        if(data_lang_change.id=="en") document.getElementById("btn_add_field_setting_lang").onclick = event => {  this.add_field_for_setting_lang();}
     }
+
+    add_field_for_setting_lang(){
+        $.MessageBox({
+            message: "Add Field for Setting language",
+            input: {key:{'type':'text','label':'Key New Language'},value:{'type':'text','label':'Value New Language'}},
+            top: "auto",
+            buttonFail: "Cancel"
+        }).done(function(data){
+            var html_new_field='<tr>';
+            html_new_field+='<td scope="col" class="w-10"><b>'+data.key+'</b></td>';
+            html_new_field+='<td scope="col" class="w-25">New key <b class="text-danger">('+data.key+')</b></td>';
+            html_new_field+='<td scope="col">';
+                html_new_field += '<div class="input-group">';
+                    html_new_field+='<input id="inp_'+data.key+'" type="text" value="'+data.value+'" class="form-control inp-lang input-sm" data-key="'+data.key+'"/>';
+                    html_new_field+='<div class="input-group-append">';
+                        html_new_field+='<button class="btn btn-outline-secondary" type="button" onclick="paste_tag(\'inp_'+data.key+'\')"><i class="fa-solid fa-paste"></i> Paste</button>';
+                        html_new_field+='<button class="btn btn-danger" type="button" onclick=" $(this).parent().parent().parent().parent().remove();"><i class="fa-solid fa-trash"></i> Delete</button>';
+                    html_new_field += '</div>'; 
+                html_new_field += '</div>';   
+            html_new_field+='</td>';
+            html_new_field+='</tr>';
+            $("#body_table_lang_setting").append(html_new_field);
+        });
+    }
+
+
 }
