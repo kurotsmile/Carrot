fc7b887d4d3c22c92a93929af5423ac28586b13c
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-07-02 08:59:21 +0700

    fix url rate box
    fix url rate box

--- assets/js/carrot_rate.js
+++ assets/js/carrot_rate.js
@@ -142,7 +142,7 @@ class Carrot_Rate{
                 var url_avatar_user_field=carrot.url()+'/images/avatar_default.png';
                 if(comment.user!=null){
                     if(comment.user.avatar!=null) url_avatar_user_field=comment.user.avatar;
-                    if(url_avatar_user_field=="") url_avatar_user_field=arrot.url()+'/images/avatar_default.png';
+                    if(url_avatar_user_field=="") url_avatar_user_field=carrot.url()+'/images/avatar_default.png';
                     html+='<img src="'+url_avatar_user_field+'" class="rounder w-100"/>';
                 }else{
                     html+='<i class="fa-solid fa-user fa-2x"></i>';
